d5b2412aafa5b9eedf2c43520f87a4cbbc1a9eac
unknown <omaralaa619@gmail.com>
2022-07-11 23:57:44 +0200

    m

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import asyncHandler from "express-async-handler";
-import User from "../models/UserModel.js";
+import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
 // Authententicate User and get Token
