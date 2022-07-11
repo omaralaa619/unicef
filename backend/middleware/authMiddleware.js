eaf70b255770d2072000dc9998446795bfe372dc
unknown <omaralaa619@gmail.com>
2022-07-11 23:53:02 +0200

    l

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 
-import User from "../models/UserModel.js";
+import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
